2ef5a7664cbc5e08022061f419619525fa70f547
Artem Derencuk <aderenchuk@natera.com>
2022-05-05 10:53:33 +0300

    add constants and fix properties

---
+++ src/main/java/com/aderenchuk/enter/constants/TourConstants.java
@@ -0,0 +1,14 @@
+package com.aderenchuk.enter.constants;
+
+public class TourConstants {
+    public static final String TOUR_ID = "TOUR_ID";
+    public static final String DIRECTION = "DIRECTION";
+    public static final String DATE_TOUR = "DATE_TOUR";
+    public static final String DATE_FROM = "dateFrom";
+    public static final String DATE_TO = "dateTo";
+
+    public static final int DIRECTION_MAX_SIZE = 50;
+
+    public static final String TOUR_NOT_FOUND = "tour not found";
+    public static final String TOUR_NOT_FOUND_BY_ID = "tour not found by id";
+}
